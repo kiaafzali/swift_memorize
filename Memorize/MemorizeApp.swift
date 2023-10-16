e3a0226064e06e820e1dab910cfed5dd2e0e90cb
KiaAfzali <kiaafzali1@gmail.com>
2023-10-16 01:11:45 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by kia on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
